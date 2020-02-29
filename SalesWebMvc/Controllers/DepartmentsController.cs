63e85acab2e90d99efa88814b385991cd7ab181f
rodrigo.shimokawa <rodrigo.shimokawa@linx.com.br>
2020-02-28 22:39:31 -0300

    Inclusao do primeiro controller departments

--- SalesWebMvc/Controllers/DepartmentsController.cs
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -10,7 +11,13 @@ namespace SalesWebMvc.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(list);
+
         }
     }
 }
