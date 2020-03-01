3c354f63508c21ce7cccaa1392d6ecdbf86ff7bc
rodrigo.shimokawa <rodrigo.shimokawa@linx.com.br>
2020-03-01 20:12:35 -0300

    delete seller

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -19,8 +19,13 @@ namespace SalesWebMvc.Models
         {
 
         }
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
-        {
+        public Seller(int id, 
+                      string name, 
+                      string email, 
+                      DateTime birthDate, 
+                      double baseSalary, 
+                      Department department)
+        {   
             Id = id;
             Name = name;
             Email = email;
